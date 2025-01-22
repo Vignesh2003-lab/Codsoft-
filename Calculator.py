d9e20588f2f562cce2810f9ec5a58c3c53a7b998
Vignesh2003-lab <collegejeppiaarengineering@gmail.com>
2025-01-22 19:23:59 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,22 @@
+def calculator(num1, num2, operator):
+    if operator == '+':
+        print("Sum:", num1 + num2)
+    elif operator == '-':
+        print("Difference:", num1 - num2)
+    elif operator == '*':
+        print("Product:", num1 * num2)
+    elif operator == '/':
+        if num2 != 0:
+            print("Quotient:", num1 / num2)
+        else:
+            print("Error: Division by zero is not allowed.")
+    else:
+        print("Invalid operator")
 
+# User inputs
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+
+# Call the function
+calculator(num1, num2, operator)
